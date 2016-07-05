080d6f61b06f09d67b736cb58db1997f05857226
TWTY1STC <liza.njuguna@gmail.com>
2016-07-05 16:55:52 +0000

    refactor revealPoints and add transition

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,26 @@
 var animatePoints = function(){
                 
     var points = document.getElementsByClassName('point');
+    var revealPoints = function(index){
+        for (var index in points){
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+    };
+    revealPoints(points);
     
-    var revealFirstPoint = function() {
+};
+
+
+
+   /*var revealFirstPoint = function() {
         points[0].style.opacity = 1;
         points[0].style.transform = "scaleX(1) translateY(0)";
         points[0].style.msTransform = "scaleX(1) translateY(0)";
         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+        points[0].style.transition = 
     };
     
     var revealSecondPoint = function() {
@@ -22,7 +36,7 @@ var animatePoints = function(){
         points[2].style.msTransform = "scaleX(1) translateY(0)";
         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
-    revealFirstPoint();
+    
+    revealFirstPoint(); 
     revealSecondPoint();
-    revealThirdPoint();
-};
+    revealThirdPoint();*/
