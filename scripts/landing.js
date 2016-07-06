787d1382d03e1de5603888aa884226421e92432c
TWTY1STC <liza.njuguna@gmail.com>
2016-07-06 15:05:38 +0000

    add scroll event to trigger selling points animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,8 @@
-var animatePoints = function(){
-                
-    var points = document.getElementsByClassName('point');
-    var revealPoints = function(points){
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points){
+    var revealPoint = function(points){
         for (var point in points){
-            console.log(point);
-            console.log(points[point]);
             points[point].style.opacity = 1;
             points[point].style.transform = "scaleX(1) translateY(0)";
             points[point].style.msTransform = "scaleX(1) translateY(0)";
@@ -12,6 +10,20 @@ var animatePoints = function(){
         }
         
     };
-    revealPoints(points);
+    revealPoint(points);
     
+};
+
+window.onload = function(){
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(pointsArray);
+        }
+    });
 };
